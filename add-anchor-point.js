15d59dd27d0396fe13916ebfc19bb0d8263a15b9
Kaifect-Luke <134304056+Kaifect-Luke@users.noreply.github.com>
2023-05-22 11:24:37 -0500

    Create add-anchor-point.js
    Use this code to add an anchor point to an element other than a block.

---
+++ add-anchor-point.js
@@ -0,0 +1,7 @@
+// Initial ID is retrieved by inspecting the checkout element. //
+
+  // Get the element
+var element = document.getElementById("checkout-widget-1674514875269");
+
+// Add the ID
+element.setAttribute("id","checkout");
